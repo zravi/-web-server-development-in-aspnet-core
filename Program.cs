1dc0d9cab57e7087201bd2d6e3dfa87d4b07be8b
TKuusisto <kuusisto.tuomo@gmail.com>
2022-09-22 16:48:15 +0300

    chore: added solution

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MovieCharactersEFCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Code first
+            
+            // Install EF tools and sqlserver
+            
+            // DbContext
+            
+            // Models
+            
+            // Connection string
+            
+            // DbSets
+            
+            // Add migration
+            
+            // Run migration
+        }
+    }
+}
